1356f15777e1174ac95f3620a3dfe59aee057eff
GUNDAMeast <strelnikov.maxim2004@gmail.com>
2025-05-04 11:48:29 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,17 +5,31 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en",
+    parser.addoption('--browser_name',
+                     action='store',
+                     default="chrome",
+                     choices=["chrome", "firefox"]
+                     help="Choose your browser: ")
+    parser.addoption('--language',
+                     action='store',
+                     default="en",
                      help="Choose your language: ")
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    options.add_argument("--log-level=3")
-    driver = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption("browser_name")
+
+    if browser_name == "chrome":
+        opts = ChromeOptions()
+        opts.add_experimental_option("prefs", {'intl.accept_languages': language})
+        driver = webdriver.Chrome(options=opts)
+    else:
+        opts = FFOptions()
+        opts.set_preference('intl.accept_languages', language)
+        driver = webdriver.Firefox(options=opts)
+
     driver.implicitly_wait(5)
     yield driver
-    print("\nquit browser..")
+    print("quit browser…")
     driver.quit()
