545bdfae3f35fad8349c3e2c18083c5fe5c8f191
GUNDAMeast <strelnikov.maxim2004@gmail.com>
2025-05-04 11:28:41 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,6 @@
+link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket"), \
+        '"Add to basket" button not found!'
