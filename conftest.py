5475afb7285daa4c6f511bea54b1736499215296
GUNDAMeast <strelnikov.maxim2004@gmail.com>
2025-05-04 11:21:52 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose your language: ")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(5)
+    yield driver
+    print("\nquit browser..")
+    driver.quit()
