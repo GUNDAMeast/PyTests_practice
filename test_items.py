c9a2b54f5b097893de0be7d622987085c10e1f40
GUNDAMeast <strelnikov.maxim2004@gmail.com>
2025-05-04 12:17:17 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,12 @@
-link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def test_guest_should_see_login_link(browser):
-    browser.get(link)
-    assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket"), \
-        '"Add to basket" button not found!'
+def test_add_to_basket_button(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(30)
+    button = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".btn-add-to-basket"))
+    )
+    assert button is not None
